d467a8481cb9772a55a1eb09a637a0968a079d92
Ging Zabala <gingzabala@gmail.com>
2017-09-30 22:28:21 +0800

    Updated addressBook.sh

--- Misc/addressBook.sh
+++ Misc/addressBook.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 dir=/Users/ging/Trainings/bash/Misc
+FILE='addressBook.txt'
+TEMPFILE='Search.txt'
 
 show_menu() {
 echo "Address Book Menu"
@@ -15,27 +17,25 @@ dir=/Users/ging/Trainings/bash/Misc
 cd /Users/ging/Trainings/bash/Misc
 
 lineno=1
-num_lines=$(cat addressBook.txt |wc -l)
-
+num_lines=$(cat ${1} |wc -l)
 
 while read LINE
-   do
-        echo "Name : $(echo ${LINE} | sed 's/ /_/g' |sed 's/:/  /g' |awk '{print
-        $1}' | sed 's/_/ /g')"
-          echo "Contact #:  $(echo ${LINE} | sed 's/ /_/g' |sed 's/:/  /g' |awk
-          '{print $2}'| sed 's/_/ /g')"
-            echo "Email Add #: $(echo ${LINE} | sed 's/ /_/g' |sed 's/:/  /g'
-            |awk '{print$3}'| sed 's/_/ /g')"
-              echo "Location: $(echo ${LINE} | sed 's/ /_/g' |sed 's/:/  /g'
-              |awk '{print $4}'|sed 's/_/ /g')"
+do
+  echo
+  echo "Name : $(echo ${LINE} | sed 's/ /_/g' |sed 's/:/  /g' |awk '{print $1}' | sed 's/_/ /g')"
+  echo "Contact #:  $(echo ${LINE} | sed 's/ /_/g' |sed 's/:/  /g' |awk '{print $2}'| sed 's/_/ /g')"
+  echo "Email Add #: $(echo ${LINE} | sed 's/ /_/g' |sed 's/:/  /g' |awk '{print$3}'| sed 's/_/ /g')"
+  echo "Location: $(echo ${LINE} | sed 's/ /_/g' |sed 's/:/  /g' |awk '{print $4}'|sed 's/_/ /g')"
+  echo
 
-                if [ ${lineno} -eq ${num_lines} ]
-                        then
-                             break
-                               fi
-                                    ((lineno++))
-                                      echo " "
-                                       done < addressBook.txt
+  if [ ${lineno} -eq ${num_lines} ]
+     then
+     break
+  fi
+     ((lineno++))
+
+ done < ${1}
+
 }
 
 # Main
@@ -47,13 +47,19 @@ show_menu
 read -p "What would you like to do? " CHOICE
 case "${CHOICE}" in
  1)
-   read -p "Type 'A' to Display All Entries or 'B' for Custom Search." ANS1
-      case "${ANS1}" in 
-      A) 
-       display
+   read -p "Type 'A' to Display All Entries or 'B' for Custom Search. " ANS1
+      case "${ANS1}" in
+      A)
+       display ${FILE}
        ;;
       B)
-       display
+       clear
+       read -p "Enter Search \"Keyword\": " SEARCH
+       #echo "Search: ${SEARCH}"
+       #echo "FILE: ${FILE}"
+       #echo "TEMPFILE: ${TEMPFILE}"
+       grep -i "${SEARCH}" ${FILE} >  ${TEMPFILE}
+       display ${TEMPFILE}
        ;;
       *)
        echo "Invalid Choice, going back to Main Menu"
@@ -61,7 +67,24 @@ case "${CHOICE}" in
       esac
    ;;
  2)
-   echo "Add"
+   clear
+   echo "*** Add New Entry ***"
+   read -p "Name      : " NEWNAME
+   read -p "Contact#  : " CONTACT
+   read -p "Email add : " EMAIL
+   read -p "Location  : " LOCATION
+
+   read -p "Do you want to save this new entry (Y/N)? " SAVE
+     case "${SAVE}" in
+        Y|y|Yes|YES|yes)
+         echo ""${NEWNAME}":"${CONTACT}":"${EMAIL}":"${LOCATION}"" >> ${FILE}
+         echo "New Entry saved."
+         echo
+         ;;
+      *)
+       echo
+       ;;
+     esac
    ;;
  3)
    echo "Remove"
@@ -70,7 +93,8 @@ case "${CHOICE}" in
    echo "Update"
    ;;
  5)
-   echo "Quit" 
+   echo "Quit"
+   rm ${TEMPFILE}
    break
    ;;
  *)
